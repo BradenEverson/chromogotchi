d010d32af12e75a624f61bc2054f59b896933e52
BradenEverson <bradeneverson@gmail.com>
2024-09-14 12:01:56 -0500

    feat: create Go types in TS

--- extension/src/handle.ts
+++ extension/src/handle.ts
@@ -1,6 +1,30 @@
-function getCookie(name: string): string | null {
+type PetId = string;
+type ErrorString = string;
+
+const HTTP_LOCATION: string = "http://localhost:7878"
+
+export function getCookie(name: string): PetId | null {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
     return null;
+}
+
+export async function newPet(): Promise<PetId | ErrorString> {
+    try {
+        const response = await fetch(HTTP_LOCATION + "/newpet", {
+            method: 'GET'
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            const petID = data.PetId;
+            document.cookie = `PETID=${petID}; path=/;`;
+            return petID as PetId;
+        } else {
+            return "Bad response from endpoint :(" as ErrorString
+        }
+    } catch (err) {
+        return "Error reaching HTTP endpoint" as ErrorString
+    }
 }
---
+++ extension/src/request.ts
@@ -0,0 +1,29 @@
+export type RequestObject = {
+    type: string,
+    data: number[]
+}
+
+export function bytesToF32(bytes: [number, number, number, number]): number {
+    let buffer = new ArrayBuffer(4);
+    let view = new DataView(buffer);
+
+    for (let index = 0; index < bytes.length; index++) {
+        view.setUint8(index, bytes[index]);
+    }
+
+    return view.getFloat32(0, true)
+}
+
+export function f32ToBytes(float: number): [number, number, number, number] {
+    let buffer = new ArrayBuffer(4);
+    let view = new DataView(buffer);
+
+    view.setFloat32(0, float)
+
+    let first = view.getUint8(0);
+    let second = view.getUint8(1);
+    let third = view.getUint8(2);
+    let fourth = view.getUint8(3);
+
+    return [first, second, third, fourth];
+}
