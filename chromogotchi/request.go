6428ce432ee402a10ca2efbb8b4e6e30858ddb14
BradenEverson <bradeneverson@gmail.com>
2024-09-14 11:27:52 -0500

    feat: serialization and deserialization of RequestObjects

--- chromogotchi/request.go
+++ chromogotchi/request.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 type RequestObject struct {
-	requestType string
-	metadata    []byte
+	RequestType string `json:"type"`
+	Metadata    []byte `json:"data"`
+}
+
+func makeRequestObject(reqtype string, data []byte) RequestObject {
+	return RequestObject{reqtype, data}
+}
+
+func deserializeRequestObject(data []byte) (*RequestObject, error) {
+	object := new(RequestObject)
+	err := json.Unmarshal(data, object)
+	if err != nil {
+		return object, nil
+	} else {
+		return nil, errors.New("Failed to deserialize request")
+	}
+}
+
+func serializeRequestObject(request *RequestObject) (*string, error) {
+	data, err := json.Marshal(request)
+	if err != nil {
+		return nil, errors.New("Failed to serialize request")
+	} else {
+		requestString := string(data)
+		return &requestString, nil
+	}
 }
--- chromogotchi/websocket.go
+++ chromogotchi/websocket.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net/http"
@@ -30,6 +32,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 }
 
 func hanndlConnection(conn *websocket.Conn) {
+	var id string
 	for {
 		messageType, message, err := conn.ReadMessage()
 		if err != nil {
@@ -39,18 +42,72 @@ func hanndlConnection(conn *websocket.Conn) {
 
 		switch messageType {
 		case websocket.TextMessage:
-			// Deserialize into a request object
+			request, err := deserializeRequestObject(message)
+			if err != nil {
+				fmt.Println(err.Error())
+				break
+			}
 
+			if request.RequestType == "Establish" {
+				id = string(request.Metadata)
+			}
+
+			if id == "" {
+				continue
+			}
+			pet := allPets[id]
+
+			var responseType string
+			var responseData []byte
+
+			var buf bytes.Buffer
+
+			switch request.RequestType {
+			case "Feed":
+				responseType = "Fed"
+				err := binary.Write(&buf, binary.LittleEndian, pet.hunger)
+				if err != nil {
+					fmt.Println(err.Error())
+					break
+				}
+				responseData = buf.Bytes()
+			case "Sleep":
+				responseType = "Slept"
+				err := binary.Write(&buf, binary.LittleEndian, pet.wakefullness)
+				if err != nil {
+					fmt.Println(err.Error())
+					break
+				}
+				responseData = buf.Bytes()
+			case "Play":
+				responseType = "Happy"
+				err := binary.Write(&buf, binary.LittleEndian, pet.depression)
+				if err != nil {
+					fmt.Println(err.Error())
+					break
+				}
+				responseData = buf.Bytes()
+			}
+
+			if responseType == "" {
+				continue
+			}
+
+			response := makeRequestObject(responseType, responseData)
+
+			newMessageType := websocket.TextMessage
+			newMessage, err := serializeRequestObject(&response)
+
+			if err == nil {
+				err = conn.WriteMessage(newMessageType, []byte(*newMessage))
+				if err != nil {
+					log.Println("Error writing message:", err)
+					break
+				}
+			} else {
+				log.Println("Error writing message:", err)
+			}
 		}
 
-		fmt.Printf("Received: %s\n", message)
-		newMessageType := websocket.TextMessage
-		newMessage := []byte("Hello")
-
-		err = conn.WriteMessage(newMessageType, newMessage)
-		if err != nil {
-			log.Println("Error writing message:", err)
-			break
-		}
 	}
 }
